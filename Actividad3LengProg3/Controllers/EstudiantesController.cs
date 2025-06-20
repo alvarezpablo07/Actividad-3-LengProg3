a110fa70ef61bde8742a7b4593b63aa1ed4f4209
alvarezpablo07 <lunap3821@gmail.com>
2025-06-19 21:29:08 -0400

    Actividad-3

---
+++ Actividad3LengProg3/Controllers/EstudiantesController.cs
@@ -0,0 +1,59 @@
+﻿using Actividad3LengProg3.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Actividad3LengProg3.Controllers
+{
+    public class EstudiantesController : Controller
+    {
+        private static List<EstudianteViewModel> estudiantes = new List<EstudianteViewModel>();
+
+        public ActionResult Index()
+        {
+            return View(new EstudianteViewModel());
+        }
+
+        [HttpPost]
+        public ActionResult Registrar(EstudianteViewModel estudiante)
+        {
+            if (ModelState.IsValid)
+            {
+                estudiantes.Add(estudiante);
+                return RedirectToAction("Lista");
+            }
+            return View("Index", estudiante);
+        }
+
+        public ActionResult Lista()
+        {
+            return View(estudiantes);
+        }
+
+        public ActionResult Editar(string matricula)
+        {
+            var estudiante = estudiantes.FirstOrDefault(e => e.Matricula == matricula);
+            return View(estudiante);
+        }
+
+        [HttpPost]
+        public ActionResult Editar(EstudianteViewModel estudiante)
+        {
+            var index = estudiantes.FindIndex(e => e.Matricula == estudiante.Matricula);
+            if (index >= 0 && ModelState.IsValid)
+            {
+                estudiantes[index] = estudiante;
+                return RedirectToAction("Lista");
+            }
+            return View(estudiante);
+        }
+
+        public ActionResult Eliminar(string matricula)
+        {
+            var estudiante = estudiantes.FirstOrDefault(e => e.Matricula == matricula);
+            if (estudiante != null)
+            {
+                estudiantes.Remove(estudiante);
+            }
+            return RedirectToAction("Lista");
+        }
+    }
+}
---
+++ Actividad3LengProg3/Models/EstudianteViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Actividad3LengProg3.Models
+{
+    public class EstudianteViewModel
+    {
+        [Required]
+        public string Matricula { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string Nombre { get; set; }
+
+        [Required]
+        [Range(1, 12)]
+        public int Semestre { get; set; }
+
+        [Required]
+        public string Carrera { get; set; }
+    }
+}
